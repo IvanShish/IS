6f996dd3c3badb03df5a272a57e46f60ef26ba28
IvanShish <rayman.4@list.ru>
2022-03-14 14:59:30 +0300

    goalie old version

--- goalieDecisionTree.js
+++ goalieDecisionTree.js
@@ -1,20 +1,16 @@
 const BALL = "b", GOAL_FLAG = "gr", MIN_X = 35, MAX_X = 52, MAX_Y = 20,
-    GOALIE_ZONE_X = 40, GOALIE_ZONE_Y = 9, CENTER_FLAG = "fc", eps = 0.5,
-    epsBall = 2.0
+    GOALIE_ZONE_X = 40, GOALIE_ZONE_Y = 9
 
 const GoalieDT = {
     state: {
         command: null,
-        sign: -1,
-        prevBallCoords: null,
-        predictBall: true,
-        predictedPoint: null
+        isBallCaught: false,
+        sign: -1
     },
 
     root: {
         exec(mgr, state) {
             state.command = null
-            state.predictedPoint = null
         },
         next: "checkBall"
     },
@@ -26,87 +22,14 @@ const GoalieDT = {
     },
 
     checkDistanceToBall: {
-        condition: (mgr, state) => mgr.getDistance(BALL) < 20,
-        trueCond: "checkBallPredict",
+        condition: (mgr, state) => mgr.getDistance(BALL) < 9 || mgr.isGoalieCloserToBall(),
+        trueCond: "closeBall",
         falseCond: "farBall"
     },
 
-    checkBallPredict: {
-        condition: (mgr, state) => {
-            const predict = state.predictBall
-            state.predictBall = true
-            return predict
-        },
-        trueCond: "needToPredictBall",
-        falseCond: "closeBall"
-    },
-
-    needToPredictBall: {
-        condition: (mgr, state) => {
-            if (!state.prevBallCoords) {
-                state.prevBallCoords = mgr.getBallCoordinates()
-                state.predictBall = false
-                return false
-            }
-            const ballCoords = mgr.getBallCoordinates()
-            const goalieCoords = mgr.getGoaliePos()
-            if (!ballCoords.x || !state.prevBallCoords.x || !goalieCoords) {
-                state.predictBall = false
-                return false
-            }
-
-            if (goalieCoords.x < MIN_X || goalieCoords.x > MAX_X ||
-            goalieCoords.y > MAX_Y || goalieCoords.y < -MAX_Y) {
-                state.predictBall = false
-                return false
-            }
-
-            if (ballCoords.x - eps >= goalieCoords.x) {
-                state.predictBall = false
-                return false
-            }
-
-            if (Math.abs(ballCoords.x - state.prevBallCoords.x) < epsBall &&
-                Math.abs(ballCoords.y - state.prevBallCoords.y) < epsBall) {
-                // console.log(ballCoords.x, state.prevBallCoords.x,
-                //     ballCoords.y, state.prevBallCoords.y)
-                state.predictBall = false
-                return false
-            }
-            const k = (ballCoords.y - state.prevBallCoords.y) /
-                (ballCoords.x - state.prevBallCoords.x)
-            const b = state.prevBallCoords.y - k * state.prevBallCoords.x
-            state.predictedPoint = k * goalieCoords.x + b
-
-            if (state.predictedPoint - eps <= goalieCoords.y &&
-                goalieCoords.y <= state.predictedPoint + eps) {
-                state.predictBall = false
-                return false
-            } else {
-                // console.log(state.predictedPoint)
-                if (state.predictedPoint < goalieCoords.y) {
-                    state.predictedAngle = 90
-                } else {
-                    state.predictedAngle = -90
-                }
-            }
-            return true
-        },
-        trueCond: "runToPredictedPoint",
-        falseCond: "closeBall"
-    },
-
-    runToPredictedPoint: {
-        exec(mgr, state) {
-            state.command = {n: "dash", v: `100 ${state.predictedAngle}`}
-        },
-        next: "sendCommand"
-    },
-
     closeBall: {
-        condition: (mgr, state) => {
-            return ((mgr.getDistance(BALL) > 1 && mgr.getDistance(BALL) < 10)) && mgr.getAngle(BALL) < 4
-        },
+        condition: (mgr, state) => mgr.getDistance(BALL) > 1 &&
+            mgr.getAngle(BALL) < 4,
         trueCond: "enemyGoalVisible",
         falseCond: "checkCatch"
     },
@@ -132,17 +55,25 @@ const GoalieDT = {
 
     rotateToEnemyGoal: {
         exec(mgr, state) {
-            if (mgr.getAngle(CENTER_FLAG) > 0)
-                state.command = {n: "kick", v: "5 -45"}
-            else
+            if (this.isBallCaught) {
+                this.isBallCaught = false
+                state.command = {n: 'turn', v: state.sign * 45}
+            } else {
                 state.command = {n: "kick", v: "5 45"}
+            }
         },
         next: "sendCommand"
     },
 
     checkCatch: {
-        condition: (mgr, state) => mgr.getDistance(BALL) <= 1,
+        condition: (mgr, state) => mgr.getDistance(BALL) <= 1 && !this.isBallCaught,
         trueCond: "checkGoalZone",
+        falseCond: "checkIsBallAlreadyCaught"
+    },
+
+    checkIsBallAlreadyCaught: {
+        condition: (mgr, state) => this.isBallCaught,
+        trueCond: "enemyGoalVisible",
         falseCond: "turnToBall"
     },
 
@@ -161,15 +92,15 @@ const GoalieDT = {
         condition: (mgr, state) => mgr.getBallCoordinates().x < MAX_X &&
             mgr.getBallCoordinates().x > MIN_X &&
             mgr.getBallCoordinates().y < MAX_Y &&
-            mgr.getBallCoordinates().y > -MAX_Y &&
-            mgr.getClosestPlayerToBallDist() < 9,
+            mgr.getBallCoordinates().y > -MAX_Y,
         trueCond: "catchBall",
         falseCond: "enemyGoalVisible"
     },
 
     catchBall: {
         exec(mgr, state) {
-            console.log('try to catch ', mgr.getAngle(BALL))
+            // console.log('catch ', mgr.getAngle(BALL))
+            this.isBallCaught = true
             state.command = {
                 n: 'catch',
                 v: mgr.getAngle(BALL)
@@ -185,19 +116,19 @@ const GoalieDT = {
     },
 
     checkDistanceToGoal: {
-        condition: (mgr, state) => {
-            const goalieCoords = mgr.getGoaliePos()
-            return mgr.getDistance(GOAL_FLAG) > 5 &&
-                goalieCoords.x > MIN_X && goalieCoords.x < MAX_X &&
-                goalieCoords.y > -MAX_Y && goalieCoords.y < MAX_Y
-        },
+        condition: (mgr, state) => mgr.getDistance(GOAL_FLAG) > 5,
         trueCond: "checkGoalAngle",
         falseCond: "closeGoal"
     },
 
     checkGoaliePos: {
-        condition: (mgr, state) => mgr.getGoaliePos().x > GOALIE_ZONE_X &&
-            -GOALIE_ZONE_Y < mgr.getGoaliePos().y && mgr.getGoaliePos().y < GOALIE_ZONE_Y,
+        condition: (mgr, state) => {
+            const goalieCoords = mgr.getGoaliePos()
+            return goalieCoords.x < MAX_X &&
+                goalieCoords.x > GOALIE_ZONE_X &&
+                -GOALIE_ZONE_Y < goalieCoords.y &&
+                goalieCoords.y < GOALIE_ZONE_Y
+        },
         trueCond: "closeGoal",
         falseCond: "rotate"
     },
