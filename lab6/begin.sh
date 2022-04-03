6d613cf0dafcfea8b7d4210a3f84be84b4363acc
MadAndrei <andreimirskov@yandex.ru>
2022-04-04 01:04:47 +0300

    add 11 players

--- lab6/begin.sh
+++ lab6/begin.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rcssserver server::coach=1 & rcssmonitor & node app.js < files/p1.txt & node app.js < files/p2.txt & node app.js < files/p3.txt & node app.js < files/p4.txt & node app.js < files/p5.txt
+rcssserver server::coach=1 & rcssmonitor & node app.js < files/p1.txt & node app.js < files/p2.txt & node app.js < files/p3.txt & node app.js < files/p4.txt & node app.js < files/p5.txt & node app.js < files/p6.txt & node app.js < files/p7.txt & node app.js < files/p8.txt & node app.js < files/p9.txt & node app.js < files/p10.txt
